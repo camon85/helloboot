af795d95da9eec26c0c6748abf60b3d380099073
camon <camon85@gmail.com>
2023-04-24 23:04:03 +0900

    서블릿 컨테이너 직접 실행

--- src/main/java/camonspring/helloboot/CamonspringApplication.java
+++ src/main/java/camonspring/helloboot/CamonspringApplication.java
@@ -1,13 +1,15 @@
 package camonspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class CamonspringApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(CamonspringApplication.class, args);
+    ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+    WebServer webServer = serverFactory.getWebServer();
+    webServer.start();
   }
 
 }
