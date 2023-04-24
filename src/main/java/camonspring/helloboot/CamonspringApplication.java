9a1953365e19ba64a9a1da32f09cdc402b67ebc2
camon <camon85@gmail.com>
2023-04-25 00:10:05 +0900

    스프링 컨테이너로 통합

--- src/main/java/camonspring/helloboot/CamonspringApplication.java
+++ src/main/java/camonspring/helloboot/CamonspringApplication.java
@@ -3,34 +3,29 @@ package camonspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class CamonspringApplication {
 
   public static void main(String[] args) {
-    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+      @Override
+      protected void onRefresh() {
+        super.onRefresh();
+
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+          servletContext.addServlet("dispatcherServlet",
+              new DispatcherServlet(this)
+          ).addMapping("/*"); // 모든 요청을 받아서 처리한다.
+        });
+        webServer.start();
+      }
+    };
     applicationContext.registerBean(HelloController.class);
     applicationContext.registerBean(SimpleHelloService.class);
     applicationContext.refresh();
-
-    ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-    WebServer webServer = serverFactory.getWebServer(servletContext -> {
-      servletContext.addServlet("dispatcherServlet",
-          new DispatcherServlet(applicationContext)
-      ).addMapping("/*"); // 모든 요청을 받아서 처리한다.
-    });
-    webServer.start();
   }
 
 }
