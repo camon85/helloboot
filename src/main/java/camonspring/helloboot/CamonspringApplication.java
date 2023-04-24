b033c89a949fa8b1b98079ec00099e2120eaf76b
camon <camon85@gmail.com>
2023-04-25 01:07:20 +0900

    SpringBootApplication

--- src/main/java/camonspring/helloboot/CamonspringApplication.java
+++ src/main/java/camonspring/helloboot/CamonspringApplication.java
@@ -1,12 +1,11 @@
 package camonspring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -24,7 +23,7 @@ public class CamonspringApplication {
   }
 
   public static void main(String[] args) {
-    MySpringApplication.run(CamonspringApplication.class, args);
+    SpringApplication.run(CamonspringApplication.class, args);
   }
 
 }
