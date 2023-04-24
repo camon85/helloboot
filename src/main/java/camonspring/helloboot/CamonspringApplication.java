55e04b6a06aa5263fb631726e5e042056c05fb80
camon <camon85@gmail.com>
2023-04-25 00:59:49 +0900

    Bean의 생명주기 메소드

--- src/main/java/camonspring/helloboot/CamonspringApplication.java
+++ src/main/java/camonspring/helloboot/CamonspringApplication.java
@@ -3,6 +3,7 @@ package camonspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,17 +13,29 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class CamonspringApplication {
 
+  @Bean
+  public ServletWebServerFactory servletWebServerFactory() {
+    return new TomcatServletWebServerFactory();
+  }
+
+  @Bean
+  public DispatcherServlet dispatcherServlet() {
+    return new DispatcherServlet();
+  }
+
   public static void main(String[] args) {
     AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
       @Override
       protected void onRefresh() {
         super.onRefresh();
 
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+        dispatcherServlet.setApplicationContext(this); // 생략해도 spring container가 주입해준다.
+
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-          servletContext.addServlet("dispatcherServlet",
-              new DispatcherServlet(this)
-          ).addMapping("/*"); // 모든 요청을 받아서 처리한다.
+          servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+              .addMapping("/*"); // 모든 요청을 받아서 처리한다.
         });
         webServer.start();
       }
