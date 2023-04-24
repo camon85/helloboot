88d4e3ce95f6e96a7d4fbf84e7b916707d1867e6
camon <camon85@gmail.com>
2023-04-24 23:21:39 +0900

    프론트컨트롤러로 전환

--- src/main/java/camonspring/helloboot/CamonspringApplication.java
+++ src/main/java/camonspring/helloboot/CamonspringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -18,15 +19,23 @@ public class CamonspringApplication {
   public static void main(String[] args) {
     ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
     WebServer webServer = serverFactory.getWebServer(servletContext -> {
-      servletContext.addServlet("hello", new HttpServlet() {
+      servletContext.addServlet("frontcontroller", new HttpServlet() {
         @Override
         protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-          String name = req.getParameter("name");
-          resp.setStatus(HttpStatus.OK.value());
-          resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-          resp.getWriter().print("Hello Servlet " + name);
+          // 인증, 보안, 다국어, 공통 기능
+          if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+            String name = req.getParameter("name");
+            resp.setStatus(HttpStatus.OK.value());
+            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+            resp.getWriter().print("Hello Servlet " + name);
+          } else if (req.getRequestURI().equals("/user")) {
+            resp.setStatus(HttpStatus.NO_CONTENT.value());
+          } else {
+            resp.setStatus(HttpStatus.NO_CONTENT.value());
+          }
+
         }
-      }).addMapping("/hello");
+      }).addMapping("/*"); // 모든 요청을 받아서 처리한다.
     });
     webServer.start();
   }
