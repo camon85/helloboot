8f92d61c8e82019b5ba31283fb22e054bcf9ac22
camon <camon85@gmail.com>
2023-04-27 22:57:46 +0900

    자동 구성 정보 파일 분리

--- src/main/java/camonspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/camonspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,25 @@
 package camonspring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+  private final ClassLoader classloader;
+
+  public MyAutoConfigImportSelector(ClassLoader classloader) {
+    this.classloader = classloader;
+  }
+
   @Override
   public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-    return new String[] {
-        "camonspring.config.autoconfig.DispatcherServletConfig",
-        "camonspring.config.autoconfig.TomcatWebServerConfig",
-    };
+    List<String> autoConfigs = new ArrayList<>();
+    ImportCandidates.load(MyAutoConfiguration.class, classloader)
+        .forEach(autoConfigs::add);
+    return autoConfigs.toArray(new String[0]);
   }
 }
