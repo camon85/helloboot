270044bb2c8163bab375e044fdcea728fed0b18f
camon <camon85@gmail.com>
2023-04-29 23:48:57 +0900

    프로퍼티 클래스 분리

---
+++ src/main/java/camonspring/config/autoconfig/ServerPropertiesConfig.java
@@ -0,0 +1,16 @@
+package camonspring.config.autoconfig;
+
+import camonspring.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@MyAutoConfiguration
+public class ServerPropertiesConfig {
+  @Bean
+  public ServerProperties serverProperties(Environment env) {
+    ServerProperties properties = new ServerProperties();
+    properties.setContextPath(env.getProperty("contextPath"));
+    properties.setPort(Integer.parseInt(env.getProperty("port")));
+    return properties;
+  }
+}
--- src/main/java/camonspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/camonspring/config/autoconfig/TomcatWebServerConfig.java
@@ -2,7 +2,6 @@ package camonspring.config.autoconfig;
 
 import camonspring.config.ConditionalMyOnClass;
 import camonspring.config.MyAutoConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -12,14 +11,12 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
-  @Value("${contextPath}")
-  String contextPath;
-
   @Bean("tomcatWebServerFactory")
   @ConditionalOnMissingBean
-  public ServletWebServerFactory servletWebServerFactory() {
+  public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
     TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-    factory.setContextPath(contextPath);
+    factory.setContextPath(properties.getContextPath());
+    factory.setPort(properties.getPort());
     return factory;
   }
 
