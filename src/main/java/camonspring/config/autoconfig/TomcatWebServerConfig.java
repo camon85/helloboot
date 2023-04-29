3fa8c2e24a4a03323bdedec8907b144b192f7cd5
camon <camon85@gmail.com>
2023-04-30 00:35:53 +0900

    EnableMyConfigurationProperties 도입

--- src/main/java/camonspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/camonspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,16 +1,16 @@
 package camonspring.config.autoconfig;
 
 import camonspring.config.ConditionalMyOnClass;
+import camonspring.config.EnableMyConfigurationProperties;
 import camonspring.config.MyAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
-@Import(ServerProperties.class)
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatWebServerConfig {
 
   @Bean("tomcatWebServerFactory")
